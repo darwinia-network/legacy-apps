2e486652cc4021ebb6b38e77c1b94a41a4492505
WoeOm <1015996366@qq.com>
2020-03-11 00:03:37 +0800

    extend darwinia network types

--- packages/types/src/interfaces/types.ts
+++ packages/types/src/interfaces/types.ts
@@ -1,5 +1,6 @@
 // Auto-generated via `yarn polkadot-types-from-defs`, do not edit
 /* eslint-disable @typescript-eslint/no-empty-interface */
+export * from '@polkadot/react-darwinia/interfaces/types';
 
 export * from './runtime/types';
 export * from './authorship/types';
