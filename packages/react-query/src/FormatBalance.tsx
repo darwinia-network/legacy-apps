b220d39af46a83e8e924f2422c9006bf1372db4a
WoeOm <1015996366@qq.com>
2020-03-12 00:16:26 +0800

    add withUnit props

--- packages/react-query/src/FormatBalance.tsx
+++ packages/react-query/src/FormatBalance.tsx
@@ -9,31 +9,33 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Compact } from '@polkadot/types';
 import { formatBalance } from '@polkadot/util';
+import { Balance } from '@polkadot/types/interfaces';
 
 import { useTranslation } from './translate';
 
 interface Props extends BareProps {
   children?: React.ReactNode;
   label?: React.ReactNode;
-  value?: Compact<any> | BN | string | null | 'all';
+  value?: Compact<any> | BN | string | Balance | null | 'all';
   withSi?: boolean;
+  withUnit?: boolean;
 }
 
 // for million, 2 * 3-grouping + comma
 const M_LENGTH = 6 + 1;
 
-function format (value: Compact<any> | BN | string, currency: string): React.ReactNode {
+function format (value: Compact<any> | BN | string | Balance, currency: string, withUnit: boolean): React.ReactNode {
   const [prefix, postfix] = formatBalance(value, { forceUnit: '-', withSi: false }).split('.');
 
   if (prefix.length > M_LENGTH) {
     // TODO Format with balance-postfix
-    return formatBalance(value);
+    return formatBalance(value, withUnit);
   }
 
   return <>{prefix}.<span className='balance-postfix'>{`000${postfix || ''}`.slice(-3)}</span> {currency}</>;
 }
 
-function formatSi (value: Compact<any> | BN | string): React.ReactNode {
+function formatSi (value: Compact<any> | BN | string | Balance, withUnit?: boolean): React.ReactNode {
   const strValue = ((value as Compact<any>).toBn ? (value as Compact<any>).toBn() : value).toString();
   const [prefix, postfix] = strValue === '0'
     ? ['0', '0']
@@ -42,13 +44,12 @@ function formatSi (value: Compact<any> | BN | string): React.ReactNode {
     ? ''
     : formatBalance.calcSi(strValue).value;
 
-  return <>{prefix}.<span className='balance-postfix'>{`000${postfix || ''}`.slice(-3)}</span>{unit === '-' ? '' : unit}</>;
+  return <>{prefix}.<span className='balance-postfix'>{`000${postfix || ''}`.slice(-3)}</span>{unit === '-' || !withUnit ? '' : unit}</>;
 }
 
-function FormatBalance ({ children, className, label, value, withSi }: Props): React.ReactElement<Props> {
+function FormatBalance ({ children, className, label, value, withSi, withUnit = false }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const [currency] = useState(formatBalance.getDefaults().unit);
-
+  const [currency] = useState(withUnit ? formatBalance.getDefaults().unit : '');
   return (
     <div className={`ui--FormatBalance ${className}`}>
       {label || ''}{
@@ -57,7 +58,7 @@ function FormatBalance ({ children, className, label, value, withSi }: Props): R
             ? t('all available')
             : withSi
               ? formatSi(value)
-              : format(value, currency)
+              : format(value, currency, withUnit)
           : '-'
       }{children}
     </div>
