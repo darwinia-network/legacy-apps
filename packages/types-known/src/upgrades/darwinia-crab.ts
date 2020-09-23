8d1d1b2082717139a6ca0ad808c500c1fef881d2
WoeOm <1015996366@qq.com>
2020-09-23 15:42:26 +0800

    add spec type for diffrent chain

---
+++ packages/types-known/src/upgrades/darwinia-crab.ts
@@ -0,0 +1,13 @@
+// Copyright 2017-2020 @polkadot/types-known authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+import { ChainUpgradesRaw } from './types';
+
+const upgrades: ChainUpgradesRaw = {
+  genesisHash: '0x34f61bfda344b3fad3c3e38832a91448b3c613b199eb23e5110a635d71c13c65',
+  versions: [
+  ]
+};
+
+export default upgrades;
