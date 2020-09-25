6ef6a01de0c69ac0e7d528741b2195593028cf4b
WoeOm <1015996366@qq.com>
2020-09-25 15:51:16 +0800

    update darwinia netork CompactAssignments type

--- packages/types-known/src/modules.ts
+++ packages/types-known/src/modules.ts
@@ -6,45 +6,7 @@ import { OverrideModuleType } from '@polkadot/types/types';
 
 // type overrides for modules (where duplication between modules exist)
 const typesModules: Record<string, OverrideModuleType> = {
-  babe: {
-    EquivocationProof: 'BabeEquivocationProof'
-  },
-  balances: {
-    Status: 'BalanceStatus'
-  },
-  contract: { // old metadata & naming
-    // v2 & v3
-    AccountInfo: 'ContractAccountInfo'
-  },
-  contracts: {
-    StorageKey: 'ContractStorageKey'
-  },
-  grandpa: {
-    Equivocation: 'GrandpaEquivocation',
-    EquivocationProof: 'GrandpaEquivocationProof'
-  },
-  identity: {
-    Judgement: 'IdentityJudgement'
-  },
-  parachains: {
-    Id: 'ParaId'
-  },
-  proposeParachain: {
-    Proposal: 'ParachainProposal'
-  },
-  proxy: {
-    Announcement: 'ProxyAnnouncement'
-  },
-  society: {
-    Judgement: 'SocietyJudgement',
-    Vote: 'SocietyVote'
-  },
-  staking: {
-    Compact: 'CompactAssignments'
-  },
-  treasury: {
-    Proposal: 'TreasuryProposal'
-  }
+
 };
 
 export default typesModules;
