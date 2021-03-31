7ff8ade83f6d3a0591dfffaac1471e0cec2c0c7d
WoeOm <1015996366@qq.com>
2021-03-31 11:45:21 +0800

    update metadata version

---
+++ packages/metadata/src/Decorated/extrinsics/index.ts
@@ -0,0 +1,8 @@
+// Copyright 2017-2020 @polkadot/metadata authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { ModulesWithCalls } from '@polkadot/types/types';
+
+const extrinsics: ModulesWithCalls = {};
+
+export default extrinsics;
