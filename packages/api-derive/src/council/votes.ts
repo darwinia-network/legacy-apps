75fad7093358f8da2e287e6ed4a19b389df7125c
sxlwar <sxlwar@163.com>
2021-08-24 10:53:45 +0800

    fix(#253): council not display

--- packages/api-derive/src/council/votes.ts
+++ packages/api-derive/src/council/votes.ts
@@ -2,89 +2,83 @@
 // This software may be modified and distributed under the terms
 // of the Apache-2.0 license. See the LICENSE file for details.
 
-import { AccountId, Balance } from '@polkadot/types/interfaces';
+import { AccountId, Balance, Voter } from '@polkadot/types/interfaces';
 import { ITuple } from '@polkadot/types/types';
-import { ApiInterfaceRx } from '@polkadot/api/types';
+import { ApiInterfaceRx, QueryableModuleStorage } from '@polkadot/api/types';
 import { DeriveCouncilVote, DeriveCouncilVotes } from '../types';
 
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Vec } from '@polkadot/types';
 
 import { memo } from '../util';
 
-function retrieveStakeOf (api: ApiInterfaceRx): Observable<[AccountId, Balance][]> {
-  const elections = (api.query.electionsPhragmen || api.query.elections);
+type VoteEntry = Voter | ITuple<[Balance, Vec<AccountId>]>;
 
-  return elections.stakeOf.creator.meta.type.asMap.linked.isTrue
-    ? elections.stakeOf<ITuple<[Vec<AccountId>, Vec<Balance>]>>().pipe(
-      map(([voters, stake]) =>
-        voters.map((voter, index): [AccountId, Balance] => [voter, stake[index]])
-      )
-    )
-    : elections.stakeOf.entries<Balance>().pipe(
-      map((entries) =>
-        entries.map(([key, stake]) => [key.args[0] as AccountId, stake])
-      )
-    );
+function isVoter (value: VoteEntry): value is Voter {
+  return !Array.isArray(value);
 }
 
-function retrieveVoteOf (api: ApiInterfaceRx): Observable<[AccountId, AccountId[]][]> {
-  const elections = (api.query.electionsPhragmen || api.query.elections);
-
-  return elections.votesOf.creator.meta.type.asMap.linked.isTrue
-    ? elections.votesOf<ITuple<[Vec<AccountId>, Vec<Vec<AccountId>>]>>().pipe(
-      map(([voters, votes]) =>
-        voters.map((voter, index) => [voter, votes[index]])
-      )
+function retrieveStakeOf (elections: QueryableModuleStorage<'rxjs'>): Observable<[AccountId, Balance][]> {
+  return elections.stakeOf.entries<Balance, [AccountId]>().pipe(
+    map((entries) =>
+      entries.map(([{ args: [accountId] }, stake]) => [accountId, stake])
     )
-    : elections.votesOf.entries<Vec<AccountId>>().pipe(
-      map((entries) =>
-        entries.map(([key, votes]) => [key.args[0] as AccountId, votes])
-      )
-    );
-}
-
-function retrievePrev (api: ApiInterfaceRx): Observable<DeriveCouncilVotes> {
-  return combineLatest([retrieveStakeOf(api), retrieveVoteOf(api)]).pipe(
-    map(([stakes, votes]): DeriveCouncilVotes => {
-      const result: DeriveCouncilVotes = [];
-
-      votes.forEach(([voter, votes]): void => {
-        result.push([voter, { stake: api.registry.createType('Balance'), votes }]);
-      });
-
-      stakes.forEach(([staker, stake]): void => {
-        const entry = result.find(([voter]) => voter.eq(staker));
-
-        if (entry) {
-          entry[1].stake = stake;
-        } else {
-          result.push([staker, { stake, votes: [] }]);
-        }
-      });
-
-      return result;
-    })
   );
 }
 
-function retrieveCurrent (api: ApiInterfaceRx): Observable<DeriveCouncilVotes> {
-  const elections = (api.query.electionsPhragmen || api.query.elections);
+function retrieveVoteOf (elections: QueryableModuleStorage<'rxjs'>): Observable<[AccountId, AccountId[]][]> {
+  return elections.votesOf.entries<Vec<AccountId>, [AccountId]>().pipe(
+    map((entries) =>
+      entries.map(([{ args: [accountId] }, votes]) => [accountId, votes])
+    )
+  );
+}
 
-  return elections.voting.entries<ITuple<[Balance, Vec<AccountId>]>>().pipe(
-    map((entries): DeriveCouncilVotes =>
-      entries.map(([key, [stake, votes]]): [AccountId, DeriveCouncilVote] =>
-        [key.args[0] as AccountId, { stake, votes }]
-      )
+function retrievePrev (api: ApiInterfaceRx, elections: QueryableModuleStorage<'rxjs'>): Observable<DeriveCouncilVotes> {
+  return combineLatest([retrieveStakeOf(elections), retrieveVoteOf(elections)]).pipe(
+    map(
+      ([stakes, votes]): DeriveCouncilVotes => {
+        const result: DeriveCouncilVotes = [];
+
+        votes.forEach(([voter, votes]): void => {
+          result.push([voter, { stake: api.registry.createType('Balance'), votes }]);
+        });
+
+        stakes.forEach(([staker, stake]): void => {
+          const entry = result.find(([voter]) => voter.eq(staker));
+
+          if (entry) {
+            entry[1].stake = stake;
+          } else {
+            result.push([staker, { stake, votes: [] }]);
+          }
+        });
+
+        return result;
+      }
+    )
+  );
+}
+
+function retrieveCurrent (elections: QueryableModuleStorage<'rxjs'>): Observable<DeriveCouncilVotes> {
+  return elections.voting.entries<VoteEntry, [AccountId]>().pipe(
+    map(
+      (entries): DeriveCouncilVotes =>
+        entries.map(([{ args: [accountId] }, value]): [AccountId, DeriveCouncilVote] => [
+          accountId,
+          isVoter(value) ? { stake: value.stake, votes: value.votes } : { stake: value[0], votes: value[1] }
+        ])
     )
   );
 }
 
 export function votes (api: ApiInterfaceRx): () => Observable<DeriveCouncilVotes> {
-  return memo((): Observable<DeriveCouncilVotes> =>
-    (api.query.electionsPhragmen || api.query.elections).stakeOf
-      ? retrievePrev(api)
-      : retrieveCurrent(api)
+  return memo(
+    (): Observable<DeriveCouncilVotes> => {
+      const elections = (api.query.phragmenElection || api.query.electionsPhragmen || api.query.elections).stakeOf;
+
+      return elections ? elections.stakeOf ? retrievePrev(api, elections) : retrieveCurrent(elections) : of([]);
+    }
   );
 }
