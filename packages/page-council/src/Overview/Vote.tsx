8a95f916289db1e33446ddffa330233bb6d81c58
sxlwar <sxlwar964@gmail.com>
2021-09-27 10:31:58 +0800

    chore: update types (#283)

--- packages/page-council/src/Overview/Vote.tsx
+++ packages/page-council/src/Overview/Vote.tsx
@@ -114,7 +114,7 @@ function Vote ({ electionsInfo }: Props): React.ReactElement<Props> {
               onStart={toggleVisible}
               params={[votes, voteValue]}
               tx={
-                api.tx.phragmenElection ? 'phragmenElection' : api.tx.electionsPhragmen
+                api.tx.phragmenElection ? 'phragmenElection.vote' : api.tx.electionsPhragmen
                   ? 'electionsPhragmen.vote'
                   : 'elections.vote'
               }
